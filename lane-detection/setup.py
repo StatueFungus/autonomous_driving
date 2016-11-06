3fba8c6212caa1447c69d465b43d75293c7e6f2f
Benjamin Christiani <benjamin.christiani@stud.h-da.de>
2016-11-06 19:11:16 +0100

    setup.py hinzugefügt, damit die detectionlib lokal verfügbar gemacht werden kann.

---
+++ lane-detection/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'Detectionlib',
+        version = 'SNAPSHOT-0.1.0',
+        description = 'Bibliothek zur Erkennung von Strassenmarkierungen auf Bildern',
+        packages=['detectionlib'],
+        author='Benjamin Christiani',
+        author_email='benjamin.christiani@stud.h-da.de'
+     )
