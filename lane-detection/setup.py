cc6e7a3a717e725c8a94b690824422e04e5f5ce0
Benjamin Christiani <benjamin.christiani@stud.h-da.de>
2016-11-06 21:45:27 +0100

    Release Version 0.1.0.

--- lane-detection/setup.py
+++ lane-detection/setup.py
@@ -6,5 +6,13 @@ setup(
         description = 'Bibliothek zur Erkennung von Strassenmarkierungen auf Bildern',
         packages=['detectionlib'],
         author='Benjamin Christiani',
-        author_email='benjamin.christiani@stud.h-da.de'
+        author_email='benjamin.christiani@stud.h-da.de',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Indended Audience :: Developers',
+            'Environment :: Ubuntu 14.04',
+            'Environment :: Ubuntu 16.04',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.7'
+        ]
      )
