391a1435a68c9053f955d4d2888df250d5d561fa
= <benjamin.christiani@stud.h-da.de>
2016-11-10 21:19:22 +0100

    lane detection Knoten umbenannt.

--- scripts/lane_detection_node.py
+++ scripts/lane_detection_node.py
@@ -26,32 +26,33 @@ class LaneDetectionNode:
 		try:
 			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 		except CvBridgeError as e:
-			rospy.loginfo(e)
+			rospy.logerr(e)
 
 		# Aufbereitung des Bilder
 		img_prep = ImagePreparator(cv_image.copy())
-		img_prep.define_roi(0.5)
+		img_prep.define_roi(0.5,0.1)
+		img_prep.filter_white_color(200,255)
 		img_prep.grayscale()
-		#img_prep.adaptive_hist_equalization()
+		img_prep.morph_open(3)
 		img_prep.blur((3,3), 0)
 		# Canny oder Threshold benutzen
-		img_prep.global_threshold(165, 255)
-		#img_prep.canny(1, 250, 3) # (1, 250, 3) oder (50, 150, 3)
+		#img_prep.global_threshold(165, 255)
+		img_prep.canny(50, 150, 3) # (1, 250, 3) oder (50, 150, 3)
 		
 		# Entdecke Linien
-		lines = self.lane_detector.houghlines_p(img_prep.image, 50, 10, 20) # (50, 10, 20) oder (100, 1, 10)
+		lines = self.lane_detector.houghlines_p(img_prep.image, 50, 10, 10) # (50, 10, 10) oder (100, 1, 10)
 		
 		# Filter korrekte Linien
-		flines = self.line_filter.filter(lines, 10, 50)
+		lines = self.line_filter.filter(lines, 10, 50)
 		
 		vis = Visualizer(cv_image)
-		vis.draw_lines(flines, (0,255,0), 2)
+		vis.draw_lines(lines, (0,255,0), 2)
 		
 		# Publish Bild mit den gezeichneten Linien
 		try:
-			self.image_pub.publish(self.bridge.cv2_to_imgmsg(vis.image, "bgr8"))
+			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
 		except CvBridgeError as e:
-			rospy.loginfo(e)
+			rospy.logerr(e)
 
 def main():
 	# Initialisiere den Knoten
@@ -59,7 +60,7 @@ def main():
 	try:
 		ld_node = LaneDetectionNode(SUB_TOPIC, PUB_TOPIC)
 	except KeyboardInterrupt:
-		rospy.loginfo("Shutting down")
+		rospy.loginfo("Shutting down node %s", NODE_NAME)
 
 if __name__ == '__main__':
 	main()
