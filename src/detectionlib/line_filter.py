c03afb26ab90d5cf90a5e1865ee18a3fa3e6eef6
= <benjamin.christiani@stud.h-da.de>
2016-11-15 23:58:19 +0100

    Linienfilter angepasst

--- src/detectionlib/line_filter.py
+++ src/detectionlib/line_filter.py
@@ -15,11 +15,36 @@ class LineFilter:
 
         '''
         self.draw_right = False
-        self.draw_left = False
+        self.valid_left_line = False
         self.height, self.width, _ = image.shape
-        self.image_x_center = self.width / 2
 
-    def filter_by_angle(self, lines, filter_angle, bla):
+    def _calc_endpoints(self, m, b):
+        endpoints = []
+        # y = m*x + b --> x = (y - b)/m
+        x1 = int((self.image_y_min - b) / m)
+        x2 = int((self.image_y_max - b) / m)
+        endpoints.append((x1, int(self.image_y_min)))
+        endpoints.append((x2, int(self.image_y_max)))
+        return endpoints
+
+    def _linear_regression(self, lines):
+        lines_x = []
+        lines_y = []
+
+        for line in lines:
+            for obj in line:
+                [x1, y1, x2, y2] = obj
+                lines_x.append(x1)
+                lines_x.append(x2)
+                lines_y.append(y1)
+                lines_y.append(y2)
+
+        if len(lines_x) > 0 and len(lines_y) > 0:
+            m, b = np.polyfit(lines_x, lines_y, 1) # y = m*x + b
+            return (m,b)
+        return None
+
+    def filter(self, lines, angle, x_center, y_min, y_max):
         filtered_lines = {'left':[], 'right':[]}
         
         # Falls die mitgegeben Linien leer sind, gebe leeres Dictionary zurück
@@ -27,6 +52,10 @@ class LineFilter:
             return filtered_lines 
         if len(lines) == 0:
             return filtered_lines
+
+        self.image_x_center = self.width * x_center
+        self.image_y_min = self.height * y_min
+        self.image_y_max = self.height * y_max
 
         right_lines = []
         left_lines = []
@@ -36,96 +65,49 @@ class LineFilter:
             left_line_obj=[]
             for obj in line:
                 [x1, y1, x2, y2] = obj
-                dx, dy = x2 - x1, y2 - y1
-                angle = np.arctan2(dy, dx) * 180 / np.pi
-                if math.fabs(angle) > filter_angle:
-                    # Filter Linien nach rechter und linker Bildhälfte
-                    if angle > 0 and x1 > self.image_x_center and x2 > self.image_x_center:
-                        right_line_obj.append(obj)
-                    elif angle < 0 and x1 < self.image_x_center and x2 < self.image_x_center:
-                        left_line_obj.append(obj)
+                # Nur Linien sind von Interesse weĺche in einem bestimmten y-Bereich liegen
+                if y1 >= self.image_y_min and y1 <= self.image_y_max and y2 >= self.image_y_min and y2 <= self.image_y_max:
+                    dx, dy = x2 - x1, y2 - y1
+                    obj_angle = np.arctan2(dy, dx) * 180 / np.pi
+                    # Filter Linien mit bestimmten Winkel
+                    if math.fabs(obj_angle) > angle:
+                        # Filter Linien nach rechter und linker Hälfte
+                        if x1 > self.image_x_center and x2 > self.image_x_center:
+                            right_line_obj.append(obj)
+                        elif x1 < self.image_x_center and x2 < self.image_x_center:
+                            left_line_obj.append(obj)
 
             if len(right_line_obj) != 0:
                 right_lines.append(right_line_obj)
             if len(left_line_obj) != 0:
                 left_lines.append(left_line_obj)
 
-        ###### LEFT ######
-        # linear regression left side
-        left_lines_x = []
-        left_lines_y = []
+        # Bilde linke Gerade mithilfe lineare Regression --> y = mx + b
+        left_line_valid = False
+        left_linear_regression = self._linear_regression(left_lines)
+        if left_linear_regression:
+            left_line_valid = True
+            left_m, left_b = left_linear_regression
 
-        for lline in left_lines:
-            for lobj in lline:
-                [x1, y1, x2, y2] = obj
-                left_lines_x.append(x1)
-                left_lines_x.append(x2)
-                left_lines_y.append(y1)
-                left_lines_y.append(y2)
+        # Bilde rechte Gerade mithilfe lineare Regression --> y = mx + b
+        right_line_valid = False
+        right_linear_regression = self._linear_regression(right_lines)
+        if right_linear_regression:
+            right_line_valid = True
+            right_m, right_b = right_linear_regression
 
-        if len(left_lines_x) > 0:
-            self.draw_left = True
-            left_m, left_b = np.polyfit(left_lines_x, left_lines_y, 1) # y = m*x + b
-            if left_m > 0:
-                self.draw_left = False
-        else:
-            self.draw_left = False
-        #### END LEFT ####
+        # Finde zwei Endpunkte für die linke und rechte Gerade
+        endpoints_right_line = []
+        endpoints_left_line = []
 
-         ###### RIGHT ######
-        # linear regression right side
-        right_lines_x = []
-        right_lines_y = []
+        if right_line_valid:
+            endpoints_right_line = self._calc_endpoints(right_m, right_b)
 
-        for rline in right_lines:
-            for robj in rline:
-                [x1, y1, x2, y2] = obj
-                right_lines_x.append(x1)
-                right_lines_x.append(x2)
-                right_lines_y.append(y1)
-                right_lines_y.append(y2)
+        if left_line_valid:
+            endpoints_left_line = self._calc_endpoints(left_m, left_b)
 
-        if len(right_lines_x) > 0:
-            self.draw_right = True
-            right_m, right_b = np.polyfit(right_lines_x, right_lines_y, 1) # y = m*x + b
-            if right_m < 0:
-                self.draw_right = False
-        else:
-            self.draw_right = False
-        #### END RIGHT ####
-
-
-        # Find 2 end points for right and left lines, used for drawing the line
-        # y = m*x + b --> x = (y - b)/m
-        res_right_line = []
-        res_left_line = []
-
-        y1 = self.height
-        y2 = int(self.height*bla)
-
-        if self.draw_right:
-            right_x1 = int((y1 - right_b) / right_m)
-            right_x2 = int((y2 - right_b) / right_m)
-
-        if self.draw_left:
-            left_x1 = int((y1 - left_b) / left_m)
-            left_x2 = int((y2 - left_b) / left_m)
-
-        if self.draw_right and (math.fabs(right_x1) > 1000000 or math.fabs(right_x2) > 1000000):
-            self.draw_right = False
-        if self.draw_left and (math.fabs(left_x1) > 1000000 or math.fabs(left_x2) > 1000000):
-            self.draw_left = False
-
-        if self.draw_right:
-            res_right_line.append((right_x1,y1))
-            res_right_line.append((right_x2, y2))
-
-        if self.draw_left:
-            res_left_line.append((left_x1,y1))
-            res_left_line.append((left_x2, y2))
-
-        filtered_lines['left'] = res_left_line
-        filtered_lines['right'] = res_right_line
+        filtered_lines['left'] = endpoints_left_line
+        filtered_lines['right'] = endpoints_right_line
 
         return filtered_lines
         
