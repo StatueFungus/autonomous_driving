a135e30669358824a9d4eb5a98a9d8ce8d53b32e
= <benjamin.christiani@stud.h-da.de>
2016-12-08 22:39:56 +0100

    Testskript an neues Python package angepasst

--- scripts/detectionlib_test.py
+++ scripts/detectionlib_test.py
@@ -3,18 +3,16 @@
 
 import cv2, os
 import numpy as np
-from detectionlib import Visualizer
-from detectionlib import LineFilter
-from detectionlib import LaneDetector
-from detectionlib import ImagePreparator
+from imagepreprocessing import Visualizer
+from imagepreprocessing import ImagePreparator
 
 def main():
     print os.getcwd()
     capture = cv2.VideoCapture('../data/road.avi')
     ticks = 0
-    
-    line_filter = LineFilter()
-    lane_detector = LaneDetector()
+
+    img_prep = ImagePreparator()
+    vis = Visualizer()
 
     while capture.isOpened():
         prevTick = ticks
@@ -23,24 +21,8 @@ def main():
         fps = int(1 / t)
         
         retval, image = capture.read()
+        height, width, channels = image.shape
         
-        # Aufbereitung des Bilder
-        img_prep = ImagePreparator(image.copy())
-        img_prep.define_roi(0.6, 0, 0.40)
-        img_prep.filter_white_color(165,255)
-        img_prep.grayscale()
-        img_prep.morph_open(3)
-        img_prep.blur((3,3), 0)
-        # Canny oder Threshold benutzen
-        #img_prep.global_threshold(165, 255)
-        img_prep.canny(50, 150, 3) # (1, 250, 3) oder (50, 150, 3)
-        
-        # Entdecke Linien
-        lines = lane_detector.houghlines_p(img_prep.image, 50, 10, 10) # (50, 10, 10) oder (100, 1, 10)
-        
-        # Filter korrekte Linien
-        lines = line_filter.filter(lines, 10)
-
         rect= np.array([
         [0, 200],
         [639, 200],
@@ -56,31 +38,14 @@ def main():
         M = cv2.getPerspectiveTransform(rect, dst)
         warped = cv2.warpPerspective(image, M, (640, 700))
 
-        img_prep2 = ImagePreparator(warped.copy())
-        #img_prep2.define_roi(0.6, 0, 0.40)
-        img_prep2.filter_white_color(165,255)
-        img_prep2.grayscale()
-        #img_prep2.morph_open(3)
-        #img_prep2.blur((3,3), 0)
+        # Aufbereitung des Bilder
+        roi = img_prep.define_roi(warped, 0.6, 0, 0.40)
+        gray = img_prep.grayscale(roi)
+        blur = img_prep.blur(gray, (5,5), 0)
+        canny = img_prep.edge_detection(blur, 50, 150, 3)
 
-        img_prep2.canny(50, 150, 3)
-
-        lines2 = lane_detector.houghlines_p(img_prep2.image, 50, 10, 10)
-
-
-        #vis = Visualizer(img_prep.image)
-        vis = Visualizer(image)
-        vis.draw_lines(lines, (0,255,0), 2)
-        vis.draw_text('FPS: ' + str(fps), 1, (255,0,0), (int(img_prep.width*0.015), int(img_prep.height*0.15)))
-        vis.show()
-
-        vis2 = Visualizer(warped)
-        vis2.draw_lines(lines2, (0,255,0), 2)
-        vis2.show("warped")
-
-        
-        vis3 = Visualizer(img_prep2.image)
-        vis3.show("prepped")
+        vis.draw_text(canny, 'FPS: ' + str(fps), 1, (255,0,0), (int(width*0.015), int(height*0.15)))
+        vis.show(canny)
 
 if __name__ == '__main__':
     main()
