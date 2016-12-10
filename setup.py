b6c071cd335762b9096278f692223645d4c954a3
= <benjamin.christiani@stud.h-da.de>
2016-12-10 16:09:28 +0100

    neues Python Paket hinzugefügt.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cameralib', 'imagepreprocessing'],
+    packages=['lanedetection', 'imagepreprocessing'],
     package_dir={'': 'src'},
 )
 
