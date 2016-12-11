0bec36b31ee5e45637be3e1a905d7e254427a528
= <benjamin.christiani@stud.h-da.de>
2016-12-11 10:22:14 +0100

    Kleine Anpassungen.

--- src/lanedetection/lane_model.py
+++ src/lanedetection/lane_model.py
@@ -3,6 +3,8 @@
 from segment_model import SegmentModel
 from lane_detector import LaneDetector
 
+SEGMENT_GAB = 10
+
 class LaneModel:
 
 	def __init__(self, lane_width, segment_count, y_offset_first_segment):
@@ -13,8 +15,7 @@ class LaneModel:
 		self.lane_detector = LaneDetector()
 
 	def update_segments(self, image):
-		for i in range(self.segment_count):
-			seg = self.segments[i]
+		for seg in self.segments:
 			seg.update_non_zero_points(image)
 			seg.left_point, seg.right_point = self.lane_detector.find_lane_points(seg)
 			seg.update_point_distance()
@@ -34,6 +35,6 @@ class LaneModel:
 	def _instantiate_segments(self):
 		seg_list = []
 		for i in range(self.segment_count):
-			y_offset_act_segment = self.y_offset_first_segment - (10 * i)
+			y_offset_act_segment = self.y_offset_first_segment - (SEGMENT_GAB * i)
 			seg_list.append(SegmentModel(y_offset_act_segment, self.lane_width))
 		return seg_list
