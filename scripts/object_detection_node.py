52d26935f07470f24286d035b1215dcb316e555a
AseYori <AseYori@outlook.de>
2016-12-15 12:16:56 +0100

    Template für object detection node und launch file erstellt.

---
+++ scripts/object_detection_node.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import cv2
+import numpy as np
+from imagepreprocessing import ImagePreparator
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+NODE_NAME = "object_detection_node"
+SUB_TOPIC = "image"
+PUB_TOPIC = ""
+QUEUE_SIZE = 10
+
+
+class ObjectDetectionNode:
+
+    def __init__(self, sub_topic, pub_topic):
+        self.bridge = CvBridge()
+        self.img_prep = ImagePreparator()
+        self.image_sub = rospy.Subscriber(sub_topic, Image, self.callback)
+        rospy.spin()
+
+    def callback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+	    copyFrame = cv_image.copy()
+        except CvBridgeError as e:
+            rospy.logerr(e)
+	
+	print "Node is running..."
+
+
+def main():
+    # Initialisiere den Knoten
+    rospy.init_node(NODE_NAME, anonymous=True)
+    try:
+        ObjectDetectionNode(SUB_TOPIC, PUB_TOPIC)
+    except KeyboardInterrupt:
+        rospy.loginfo("Shutting down node %s", NODE_NAME)
+
+if __name__ == '__main__':
+    main()
