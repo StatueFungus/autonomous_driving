08b7c5cc77b1bf7f565041878682b0e66d48172e
= <benjamin.christiani@stud.h-da.de>
2016-12-23 09:52:17 +0100

    Relevante Parameter fürs Lane Tracking ins Launch File ausgelagert und default Werte in den Knoten hinzugefügt.

--- scripts/lane_tracking_node.py
+++ scripts/lane_tracking_node.py
@@ -14,13 +14,20 @@ PUB_TOPIC = "debug_image"
 PUB_SETPOINT_TOPIC = "setpoint"
 PUB_STATE_TOPIC = "state"
 QUEUE_SIZE = 1
+# valide default Werte für eine Bildauflösung von 320x240
+DEFAULT_LANE_WIDTH = 20
+DEFAULT_SEGMENT_START = 210
+DEFAULT_SEGMENT_AMOUNT = 1
 
 
 class LaneTrackingNode:
 
     def __init__(self, node_name, sub_topic, pub_topic, pub_setpoint_topic, pub_state_topic):
         self.bridge = CvBridge()
-        self.lane_model = LaneModel(20, 1, 210)
+        lane_width = rospy.get_param("/autonomous_driving/lane_tracking_node/lane_width", DEFAULT_LANE_WIDTH)
+        segment_start = rospy.get_param("/autonomous_driving/lane_tracking_node/segment_start", DEFAULT_SEGMENT_START)
+        segment_amount = rospy.get_param("/autonomous_driving/lane_tracking_node/segment_amount", DEFAULT_SEGMENT_AMOUNT)
+        self.lane_model = LaneModel(lane_width, segment_amount, segment_start)
         self.image_pub = rospy.Publisher(pub_topic, Image, queue_size=QUEUE_SIZE)
         self.setpoint_pub = rospy.Publisher(pub_setpoint_topic, Float64, queue_size=QUEUE_SIZE)
         self.state_pub = rospy.Publisher(pub_state_topic, Float64, queue_size=QUEUE_SIZE)
