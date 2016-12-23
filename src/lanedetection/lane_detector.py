b27ba07e3855ede8db848282815b840fa6c8d69a
= <benjamin.christiani@stud.h-da.de>
2016-12-23 16:35:37 +0100

    Refactoring und Dokumentation Lane Tracking Algorithmus.

--- src/lanedetection/lane_detector.py
+++ src/lanedetection/lane_detector.py
@@ -11,63 +11,58 @@ class LaneDetector:
         self.lane_width_tolerance = round(self.lane_width * WIDTH_TOLERANCE)
 
     def find_lane_points(self, segment):
-        left_points = segment.nz_left_points
-        right_points = segment.nz_right_points
-        line_distance = segment.point_distance
+        point_distance = segment.point_distance
         segment_center = segment.point_center
-        left_point = segment.left_point  # Könnte None sein
-        right_point = segment.right_point  # Könnte None sein
 
-        left_points_score = {}
-        for idx, p in enumerate(left_points):
-            left_points_score[p] = 0
-            # ist der Punkt der nächste zur Segmentmitte?
-            if idx == 0:
-                left_points_score[p] += 3
-            # besitzt der Punkt einen Nachbarpunkt?
-            if len(left_points) > idx + 1 and left_points[idx + 1] in range(p-2, p):
-                left_points_score[p] += 1
-            # liegt der Punkt im Bereich seines Vorgängers?
-            if left_point is not None and p in range(left_point - 4, left_point + 4):
-                left_points_score[p] += 2
+        # kalkuriere Scoring Table
+        left_points_score = self._calc_point_score(segment.nz_left_points, segment.left_point)
+        right_points_score = self._calc_point_score(segment.nz_right_points, segment.right_point)
 
-        right_points_score = {}
-        for idx, p in enumerate(right_points):
-            right_points_score[p] = 0
-            # ist der Punkt der nächste zur Segmentmitte?
-            if idx == 0:
-                right_points_score[p] += 3
-            # besitzt der Punkt einen Nachbarpunkt?
-            if len(right_points) > idx + 1 and right_points[idx + 1] in range(p, p+2):
-                right_points_score[p] += 1
-            # liegt der Punkt im Bereich seines Vorgängers?
-            if right_point is not None and p in range(right_point - 4, right_point + 4):
-                right_points_score[p] += 2
+        # definiere linken und rechten Kandidaten
+        left_candidate = self._define_candidate(left_points_score, segment_center, point_distance, left=True)
+        right_candidate = self._define_candidate(right_points_score, segment_center, point_distance, left=False)
 
-        if left_points_score:
-            left_candidate = max(left_points_score, key=left_points_score.get)
-        else:
-            left_candidate = segment_center + round(line_distance / 2)
-        if right_points_score:
-            right_candidate = max(right_points_score, key=right_points_score.get)
-        else:
-            right_candidate = segment_center - round(line_distance / 2)
+        # validiere linken und rechten Kandidaten
+        left_candidate_valid = self._validate_candidate(left_candidate, segment_center, point_distance)
+        right_candidate_valid = self._validate_candidate(right_candidate, segment_center, point_distance)
 
-        res_left_candidate = self._validate_candidate(left_candidate, segment_center, line_distance)
-        res_right_candidate = self._validate_candidate(right_candidate, segment_center, line_distance)
-
-        if res_left_candidate and res_right_candidate:
+        # definiere linken und rechten Punkt
+        if left_candidate_valid and right_candidate_valid:
             return int(left_candidate), int(right_candidate)
-        elif res_left_candidate and not res_right_candidate:
-            return int(left_candidate), int(left_candidate + line_distance)
-        elif not res_left_candidate and right_candidate:
-            return int(right_candidate - line_distance), int(right_candidate)
+        elif left_candidate_valid and not right_candidate_valid:
+            return int(left_candidate), int(left_candidate + point_distance)
+        elif not left_candidate_valid and right_candidate_valid:
+            return int(right_candidate - point_distance), int(right_candidate)
         else:
-            return left_point, right_point
+            return segment.left_point, segment.right_point
 
-    def _validate_candidate(self, new_point, segment_center, line_distance):
+    def _define_candidate(self, point_score, segment_center, point_distance, left):
+        if point_score:
+            return max(point_score, key=point_score.get)
+        if left:
+            return segment_center + round(point_distance / 2)
+        else:
+            return segment_center - round(point_distance / 2)
+
+    def _calc_point_score(self, points, old_point):
+        point_score = {}
+
+        for idx, point in enumerate(points):
+            point_score[point] = 0
+            # ist der Punkt der nächste zur Segmentmitte?
+            if idx == 0:
+                point_score[point] += 3
+            # liegt der Punkt im Bereich seines Vorgängers?
+            if old_point is not None and point in range(old_point - 4, old_point + 4):
+                point_score[point] += 2
+            # besitzt der Punkt einen Nachbarpunkt?
+            if len(points) > idx + 1 and points[idx + 1] in range(point-2, point+2):
+                point_score[point] += 1
+        return point_score
+
+    def _validate_candidate(self, point, segment_center, line_distance):
         """ Checkt, ob ein Kandidat in einem sinvollen Abstand zur vorherigen Segmentmitte liegt """
-        distance_seg_center = abs(new_point - segment_center)
+        distance_seg_center = abs(point - segment_center)
         diff_distance_seg_center = abs(distance_seg_center - round(line_distance / 2))
         if diff_distance_seg_center <= self.lane_width_tolerance:
             return True
