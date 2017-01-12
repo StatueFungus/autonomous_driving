8dba1be8cb8f60d820891cf01058140ee4e95419
AseYori <AseYori@outlook.de>
2017-01-12 20:04:58 +0100

    resizedImage durch cv_image ersetzt, opencv imshow auskommentiert

--- scripts/object_detection_node.py
+++ scripts/object_detection_node.py
@@ -16,17 +16,17 @@ DEFAULT_BREAKING_DISTANCE = 100
 
 waitValue = 1
 
-cv2.namedWindow("original")
-cv2.moveWindow("original", 650,350)
+## cv2.namedWindow("original")
+## cv2.moveWindow("original", 650,350)
 
-cv2.namedWindow("theMask")
-cv2.moveWindow("theMask",390,350)
-cv2.namedWindow("hsvMask")
-cv2.moveWindow("hsvMask",390,50)
-cv2.namedWindow("hsvMask2")
-cv2.moveWindow("hsvMask2",720,50)
-cv2.namedWindow("hsvMask3")
-cv2.moveWindow("hsvMask3",1050,50)
+## cv2.namedWindow("theMask")
+## cv2.moveWindow("theMask",390,350)
+## cv2.namedWindow("hsvMask")
+## cv2.moveWindow("hsvMask",390,50)
+## cv2.namedWindow("hsvMask2")
+## cv2.moveWindow("hsvMask2",720,50)
+## cv2.namedWindow("hsvMask3")
+## cv2.moveWindow("hsvMask3",1050,50)
 ## cv2.namedWindow("gamma2")
 ## cv2.moveWindow("gamma2",1,50)
 ## cv2.namedWindow("gamma3")
@@ -69,16 +69,18 @@ class ObjectDetectionNode:
 	
 	
 	## resize the Image from 640*480 to 320*240
-	resizedImage = cv2.resize(cv_image.copy(),None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+	#resizedImage = cv2.resize(cv_image.copy(),None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
 	
-	videoHeight, videoWidth , _ = resizedImage.shape
+	#videoHeight, videoWidth , _ = resizedImage.shape
+	videoHeight, videoWidth , _ = cv_image.shape
 	
 	## create a blank Image for the Mask
-	copyFrame = np.zeros(resizedImage.shape, np.uint8)
+	#copyFrame = np.zeros(resizedImage.shape, np.uint8)
+	copyFrame = np.zeros(cv_image.shape, np.uint8)
 	## img[y: y + h, x: x + w]
 	## crop the Image and store it to the blank image
-	copyFrame[(videoHeight/2) : videoHeight, 0: videoWidth] = resizedImage[(videoHeight/2) : videoHeight, 0: videoWidth] # Crop from x, y, w, h -> 100, 200, 300, 400
-	
+	#copyFrame[(videoHeight/2) : videoHeight, 0: videoWidth] = resizedImage[(videoHeight/2) : videoHeight, 0: videoWidth] # Crop from x, y, w, h -> 100, 200, 300, 400
+	copyFrame[(videoHeight/2) : videoHeight, 0: videoWidth] = cv_image[(videoHeight/2) : videoHeight, 0: videoWidth] # Crop from x, y, w, h -> 100, 200, 300, 400
 	## blur the image to remove noise
 	##Knoten
 	copyFrame = cv2.bilateralFilter(copyFrame, 5, 90,40)
@@ -101,7 +103,7 @@ class ObjectDetectionNode:
 	gamma2 = correct_gamma(copyFrame,0.20)
 	hsvMask2 = cv2.inRange(cv2.cvtColor(gamma2, cv2.COLOR_BGR2HSV), lower_color, upper_color) # 0.32
 	## threshold the gamma corrected (makes the image less darker then the previous one) image to get darker spots of the yellow object
-	gamma3 = correct_gamma(copyFrame,0.48)#35#50
+	gamma3 = correct_gamma(copyFrame,0.50)#35#50
 	hsvMask3 = cv2.inRange(cv2.cvtColor(gamma3, cv2.COLOR_BGR2HSV), lower_color, upper_color)
 	
 	## add all hsv masks toghter
@@ -126,20 +128,22 @@ class ObjectDetectionNode:
 			## get the moments and draw the contour
 			mom = cv2.moments(cnt)
 			#cv2.circle(resizedImage , (int(mom['m10']/mom['m00']) , int(mom['m01']/mom['m00'])),int(np.sqrt(cv2.contourArea(cnt)/np.pi)+0.5),(0,0,255),2)
-			cv2.circle(resizedImage, (int(mom['m10']/mom['m00']) , int(mom['m01']/mom['m00'] + (np.sqrt(cv2.contourArea(cnt)/np.pi)))), 1, (20,0,255),2)
+			#cv2.circle(resizedImage, (int(mom['m10']/mom['m00']) , int(mom['m01']/mom['m00'] + (np.sqrt(cv2.contourArea(cnt)/np.pi)))), 1, (20,0,255),2)
+			cv2.circle(cv_image, (int(mom['m10']/mom['m00']) , int(mom['m01']/mom['m00'] + (np.sqrt(cv2.contourArea(cnt)/np.pi)))), 1, (20,0,255),2)
 			
 			## calculate the distance from the car to the object in pixels
 			distance = videoHeight - int(mom['m01']/mom['m00'] + (np.sqrt(cv2.contourArea(cnt)/np.pi)))
-			#print("Distance: " + str(distance) + " pixel")
+			print("Distance: " + str(distance) + " pixel")
 			if minDistance > distance:
 				minDistance = distance
 				centerX = int(mom['m10']/mom['m00'])
 	
-	cv2.imshow("original", resizedImage)
-	cv2.imshow("theMask", theMask)
-	cv2.imshow("hsvMask", hsvMask)
-	cv2.imshow("hsvMask2", hsvMask2)
-	cv2.imshow("hsvMask3", hsvMask3)
+	#cv2.imshow("original", resizedImage)
+	## cv2.imshow("original", cv_image)
+	## cv2.imshow("theMask", theMask)
+	## cv2.imshow("hsvMask", hsvMask)
+	## cv2.imshow("hsvMask2", hsvMask2)
+	## cv2.imshow("hsvMask3", hsvMask3)
 	#cv2.imshow("gamma3",gamma3)
 	#cv2.imshow("gamma2",gamma2)
 
