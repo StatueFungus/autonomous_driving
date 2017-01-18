69621eeab5d67a5a6242321b85037aeb316ecd39
Benedikt Suessmann <benedikt.suessmann@googlemail.com>
2017-01-18 11:56:17 +0100

    Reset Funktion für Performace Tracking eingebaut

--- scripts/lane_detection_node.py
+++ scripts/lane_detection_node.py
@@ -6,6 +6,7 @@ from imagepreprocessing import ImagePreparator
 from imagepreprocessing import InversePerspectiveMapping
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty, EmptyResponse
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import rospy
@@ -15,6 +16,8 @@ SUB_TOPIC = "image"
 PUB_TOPIC = "debug_image"
 PUB_SETPOINT_TOPIC = "setpoint"
 PUB_STATE_TOPIC = "state"
+RESET_SERVICE = "reset"
+
 QUEUE_SIZE = 1
 
 # valide default Werte für eine Bildauflösung von 320x240
@@ -22,9 +25,10 @@ DEFAULT_LANE_WIDTH = 20
 DEFAULT_SEGMENT_START = 210
 DEFAULT_SEGMENT_AMOUNT = 1
 
+
 class LaneDetectionNode:
 
-    def __init__(self, node_name, sub_topic, pub_topic,  pub_setpoint_topic, pub_state_topic):
+    def __init__(self, node_name, sub_topic, pub_topic,  pub_setpoint_topic, pub_state_topic, reset_service):
         self.bridge = CvBridge()
         self.img_prep = ImagePreparator()
         self.ipm = InversePerspectiveMapping()
@@ -34,9 +38,12 @@ class LaneDetectionNode:
         self.image_pub = rospy.Publisher(pub_topic, Image, queue_size=QUEUE_SIZE)
         self.setpoint_pub = rospy.Publisher(pub_setpoint_topic, Float64, queue_size=QUEUE_SIZE)
         self.state_pub = rospy.Publisher(pub_state_topic, Float64, queue_size=QUEUE_SIZE)
-        
+
+        self.reset_srv = rospy.Service(reset_service, Empty, self.reset_callback)
+        self.reset_tracking = False
+
         rospy.init_node(node_name, anonymous=True)
-        
+
         self.image_sub = rospy.Subscriber(sub_topic, Image, self.callback)
 
         # Crop Parameters
@@ -54,10 +61,7 @@ class LaneDetectionNode:
         self.aperture = rospy.get_param("/autonomous_driving/lane_detection_node/aperture", 3)
 
         # Lane Tracking
-        lane_width = rospy.get_param("/autonomous_driving/lane_detection_node/lane_width", DEFAULT_LANE_WIDTH)
-        segment_start = rospy.get_param("/autonomous_driving/lane_detection_node/segment_start", DEFAULT_SEGMENT_START)
-        segment_amount = rospy.get_param("/autonomous_driving/lane_detection_node/segment_amount", DEFAULT_SEGMENT_AMOUNT)
-        self.lane_model = LaneModel(lane_width, segment_amount, segment_start)
+        self.init_lanemodel()
 
         rospy.spin()
 
@@ -89,19 +93,35 @@ class LaneDetectionNode:
         self.lane_model.draw_segments(canny)
         state_point_x = self.lane_model.state_point_x()
 
+        if self.reset_tracking is True:
+            self.init_lanemodel()
+            self.reset_tracking = False
+
         # publish to pid
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(canny, "bgr8"))
             self.setpoint_pub.publish(0.0)
             if state_point_x:
-		heigth, width, _ = canny.shape
+                heigth, width, _ = canny.shape
                 self.state_pub.publish(state_point_x - int(width/2))
         except CvBridgeError as e:
             rospy.logerr(e)
 
+    def init_lanemodel(self):
+        lane_width = rospy.get_param("/autonomous_driving/lane_tracking_node/lane_width", DEFAULT_LANE_WIDTH)
+        segment_start = rospy.get_param("/autonomous_driving/lane_tracking_node/segment_start", DEFAULT_SEGMENT_START)
+        segment_amount = rospy.get_param("/autonomous_driving/lane_tracking_node/segment_amount", DEFAULT_SEGMENT_AMOUNT)
+        self.lane_model = LaneModel(lane_width, segment_amount, segment_start)
+
+    def reset_callback(self, req):
+        rospy.loginfo("Reset Lanetracking")
+        self.reset_tracking = True
+        return EmptyResponse()
+
+
 def main():
     try:
-        LaneDetectionNode(NODE_NAME, SUB_TOPIC, PUB_TOPIC, PUB_SETPOINT_TOPIC, PUB_STATE_TOPIC)
+        LaneDetectionNode(NODE_NAME, SUB_TOPIC, PUB_TOPIC, PUB_SETPOINT_TOPIC, PUB_STATE_TOPIC, RESET_SERVICE)
     except KeyboardInterrupt:
         rospy.loginfo("Shutting down node %s", NODE_NAME)
 
